2d6268927ffd8652679ce09cbebbd4ce18cbee5a
joecole1011 <akawit_7@hotmail.com>
2021-04-17 00:12:24 +0700

    Templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core' ;
+    
+    @Component ( {
+        selector : 'app-home',
+        templateUrl : './home.component.html' 
+        
+    } )
+
+    export class HomeComponent {
+
+        @Input()
+        title!: string; 
+        //title = '';
+    
+    }
